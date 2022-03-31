c3758bfe404852c5d9943af5536a9e3a9ca5d3c2
Pham Quang Huy <phamquanghuy25695@example.com>
2022-03-31 11:57:27 +0700

    file

---
+++ src/main/java/com/phamhuy/repository/ICustomerRepository.java
@@ -0,0 +1,9 @@
+package com.phamhuy.repository;
+
+import com.phamhuy.model.Customer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+}
---
+++ src/main/java/com/phamhuy/service/ICustomerService.java
@@ -0,0 +1,6 @@
+package com.phamhuy.service;
+
+import com.phamhuy.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
